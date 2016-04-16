416ea9b5f0401651292cd85a83fc8cf9519e950d
Karol Sotomski <karol.sotomski@gmail.com>
2016-04-16 13:37:30 +0200

    initial commit

---
+++ RaspberryHelloWorld/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace RaspberryHelloWorld
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("RaspberryHelloWorld")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("sotomski")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
