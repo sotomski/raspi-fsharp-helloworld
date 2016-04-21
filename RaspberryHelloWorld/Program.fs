0252d5ac3f2e08b95caac4a138843ae0795248bf
Karol Sotomski <karol.sotomski@gmail.com>
2016-04-21 21:41:18 +0200

    WiringPi works with F#!
    First successful experiment with WiringPi

--- RaspberryHelloWorld/Program.fs
+++ RaspberryHelloWorld/Program.fs
@@ -2,8 +2,35 @@
 // See the 'F# Tutorial' project for more help.
 module MainModule
 
+open WiringPiWrapper
+open System.Threading
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "Setting up wiringPi"
+    wiringPiSetup()
+
+    printfn "Setting up the pin"
+    pinMode pinId.wipi0 Out
+
+    printfn "Pin High"
+    digitalWrite pinId.wipi0 High
+    printfn "Sleep 1000"
+    Thread.Sleep(1000)
+    printfn "Pin Low"
+    digitalWrite pinId.wipi0 Low
+    printfn "Sleep 1000"
+    Thread.Sleep(1000)
+    printfn "Pin High"
+    digitalWrite pinId.wipi0 High
+    printfn "Sleep 1000"
+    Thread.Sleep(1000)
+    printfn "Pin Low"
+    digitalWrite pinId.wipi0 Low
+    printfn "Sleep 1000"
+    Thread.Sleep(1000)
+    printfn "Pin High"
+    digitalWrite pinId.wipi0 High
+
     0 // return an integer exit code
 
